22347576a4c8c7986dc849c64357b5d79cb27bae
Amir <amirhatam136@gmail.com>
2022-12-21 11:09:37 +0100

    Cannot find name 'describe'. Do you need to install type definitions

---
+++ wns-test-display-name/src/getDisplayName.ts
@@ -0,0 +1,10 @@
+const getDisplayName = (
+  firstName: string,
+  lastName: string,
+  city: string = ""
+) => {
+  const mayBeCity = city || "?";
+  return `[${mayBeCity}] ${firstName} ${lastName}`;
+};
+
+export default getDisplayName;
